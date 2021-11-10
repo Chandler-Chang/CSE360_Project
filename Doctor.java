1ef941c8de7663dbf017be03e0f0aaf6e0c1e9f7
Gordon Turner <94064982+gtturne1@users.noreply.github.com>
2021-11-10 10:23:28 -0700

    Updated Nurse/Doctor classes
    Commented code to ease readability and cleaned up whitespace

--- Doctor.java
+++ Doctor.java
@@ -1,7 +1,11 @@
-
+// Doctor is a type of Nurse
+// Takes all of Nurse functions and adds to them
 class Doctor extends Nurse {
 	private Nurse[] assignedNurses;
 	
+	
+	// Default Doctor Constructor
+	// Default Nurse Array is equal to 10 nurses/doctor
 	public Doctor() {
 		this.firstName = "";
 		this.lastName = "";
@@ -11,6 +15,8 @@ class Doctor extends Nurse {
 		initNurseList();
 	}
 	
+	
+	// Constructor of Doctor first and last name, user name and password
 	public Doctor(String First, String Last, String uname, String passwd) {
 		this.firstName = First;
 		this.lastName = Last;
@@ -20,6 +26,7 @@ class Doctor extends Nurse {
 		initNurseList();
 	}
 	
+	// Initialize the nurse array to all be null
 	public void initNurseList() {
 		int length = assignedNurses.length;
 		for (int i = 0; i < length; i++) {
@@ -27,44 +34,54 @@ class Doctor extends Nurse {
 		}
 	}
 	
+	// Add a singular nurse to the nurse array
 	public boolean addNurse(Nurse newNurse) {
 		int i = 0;
 		while (this.assignedNurses[i] != null) {
+			// If the doctor's nurse array is full, cannot add nurse, return false
 			if (i == this.assignedNurses.length) {
 				System.out.println("This doctor has too many nurses!");
 				return false;
 			}
+			// If nurse currently assigned to this doctor, cannot add nurse, return false
 			if (newNurse == this.assignedNurses[i]) {
 				System.out.println("Nurse already exists!");
 				return false;
 			}
 			i++;
 		}
+		// Add the nurse to first null element of the array
 		this.assignedNurses[i] = newNurse;
 		return true;
 	}
 	
+	// Searches nurse by first name and returns index of array they are at
 	public int searchNurse(String nurseFirst) {
 		int i = 0;
 		while (this.assignedNurses[i] != null) {
 			if(this.assignedNurses[i].firstName.equals(nurseFirst))
-				return i;
+				return i; // Nurse is found, return the index they are at
 			i++;
 		}
+		// Nurse does not exist in this array.
 		System.out.println(nurseFirst + " not found");
 		return -1;
 	}
 	
+	// Searches for nurse and removes her from the array
 	public boolean removeNurse(String firstName) {
 		int removalIndex = searchNurse(firstName);
 		if (removalIndex == -1) 
-			return false;
+			return false; // Cannot remove a non-existent nurse
+		
+		// Shift the array to the left and thus remove the nurse's reference
 		for (int i = removalIndex; i < this.assignedNurses.length - 1; i++)
 			this.assignedNurses[i] = this.assignedNurses[i + 1];
 		
 		return true;
 	}
 	
+	// Print the array of nurses for the given doctor
 	public void printNurses() {
 		System.out.println(this.firstName + " has the following nurses:");
 		int i = 0;
@@ -74,9 +91,8 @@ class Doctor extends Nurse {
 		}
 	}
 	
-	public Nurse[] getAssignedNurses() {
-		return this.assignedNurses;
-	}
+	// Return the nurse array
+	public Nurse[] getAssignedNurses() { return this.assignedNurses; }
 	
 	
 }
--- Nurse.java
+++ Nurse.java
@@ -1,9 +1,14 @@
-
+// Nurse Class
+// Defines all features that a nurse should implement
 class Nurse {
+	// Each nurse/doctor has all of these
 	protected String firstName, lastName;
 	protected String username, password;
+	
+	// Only nurse has assigned doctor
 	private Doctor assignedDoc;
 
+	// Default Nurse Constructor
 	public Nurse() {
 		this.firstName = "";
 		this.lastName = "";
@@ -12,6 +17,7 @@ class Nurse {
 		this.assignedDoc = null;
 	}
 	
+	// Constructor of Nurse first and last name, user name and password
 	public Nurse(String First, String Last, String uname, String passwd) {
 		this.firstName = First;
 		this.lastName = Last;
@@ -20,43 +26,39 @@ class Nurse {
 		this.assignedDoc = null;
 	}
 
-	public void setFirst(String First) {
-		this.firstName = First;
-	}
+	// Change Nurse's first name
+	public void setFirst(String First) { this.firstName = First; }
 
-	public void setLast(String Last) {
-		this.lastName = Last;
-	}
+	// Change Nurse's last name
+	public void setLast(String Last) { this.lastName = Last; }
 
-	public void setUserName(String uname) {
-		this.username = uname;
-	}
+	
+	// Change Nurse's user name
+	public void setUserName(String uname) { this.username = uname; }
 
-	public void setPassword(String passwd) {
-		this.password = passwd;
-	}
+	// Change Nurse's password
+	public void setPassword(String passwd) { this.password = passwd; }
 
-	public void setDoc(Doctor newDoc) {
-		this.assignedDoc = newDoc;
-	}
+	
+	// Change Nurse's assigned doctor
+	public void setDoc(Doctor newDoc) { this.assignedDoc = newDoc; }
 
-	public String getFirst() {
-		return this.firstName;
-	}
+	
+	
+	// Returns Nurse's first name
+	public String getFirst() { return this.firstName; }
 
-	public String getLast() {
-		return this.lastName;
-	}
+	// Returns Nurse's last name
+	public String getLast() { return this.lastName; }
 
-	public String getUserName() {
-		return this.username;
-	}
+	
+	// Returns Nurse's user name
+	public String getUserName() { return this.username; }
 
-	public String getPassword() {
-		return this.password;
-	}
+	// Returns Nurse's password
+	public String getPassword() { return this.password; }
 
-	public Doctor getDoc() {
-		return this.assignedDoc;
-	}
+	
+	// Returns Nurse's assigned doctor
+	public Doctor getDoc() { return this.assignedDoc; }
 }
