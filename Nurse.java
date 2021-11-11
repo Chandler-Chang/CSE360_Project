8d695e0290a0ae37dd80cb10146589f256d0a796
Gordon Turner <94064982+gtturne1@users.noreply.github.com>
2021-11-10 18:13:40 -0700

    Updated Person/Nurse Classes
    Extended Nurse class to implement functions included in the Person class and edited the Person class to be more robust

--- Nurse.java
+++ Nurse.java
@@ -1,10 +1,6 @@
 // Nurse Class
 // Defines all features that a nurse should implement
-class Nurse {
-	// Each nurse/doctor has all of these
-	protected String firstName, lastName;
-	protected String username, password;
-	
+class Nurse extends Person {
 	// Only nurse has assigned doctor
 	private Doctor assignedDoc;
 
@@ -17,7 +13,7 @@ class Nurse {
 		this.assignedDoc = null;
 	}
 	
-	// Constructor of Nurse first and last name, user name and password
+	// Constructor of Nurse with first and last name, user name and password
 	public Nurse(String First, String Last, String uname, String passwd) {
 		this.firstName = First;
 		this.lastName = Last;
@@ -26,37 +22,9 @@ class Nurse {
 		this.assignedDoc = null;
 	}
 
-	// Change Nurse's first name
-	public void setFirst(String First) { this.firstName = First; }
-
-	// Change Nurse's last name
-	public void setLast(String Last) { this.lastName = Last; }
-
-	
-	// Change Nurse's user name
-	public void setUserName(String uname) { this.username = uname; }
-
-	// Change Nurse's password
-	public void setPassword(String passwd) { this.password = passwd; }
-
 	
 	// Change Nurse's assigned doctor
 	public void setDoc(Doctor newDoc) { this.assignedDoc = newDoc; }
-
-	
-	
-	// Returns Nurse's first name
-	public String getFirst() { return this.firstName; }
-
-	// Returns Nurse's last name
-	public String getLast() { return this.lastName; }
-
-	
-	// Returns Nurse's user name
-	public String getUserName() { return this.username; }
-
-	// Returns Nurse's password
-	public String getPassword() { return this.password; }
 
 	
 	// Returns Nurse's assigned doctor
--- Person.java
+++ Person.java
@@ -1,26 +1,50 @@
 
 public class Person {
 
-	protected String name;
-	protected String username;
+	// Each person has first/last name, user name and password
+	protected String firstName, lastName;
+	protected String username, password;
 	
-	public Person(String name) {
-		this.name = name;
+	// Default Person Constructor
+	public Person() {
+		this.firstName = "";
+		this.lastName = "";
+		this.username = "";
+		this.password = "";
+	}
+		
+	// Constructor of Person with first and last name, user name and password
+	public Person(String First, String Last, String uname, String passwd) {
+		this.firstName = First;
+		this.lastName = Last;
+		this.username = uname;
+		this.password = passwd;
 	}
 	
-	public String getName() {
-		return name;
-	}
+	// Change Person's first name
+	public void setFirst(String First) { this.firstName = First; }
+
+	// Change Person's last name
+	public void setLast(String Last) { this.lastName = Last; }
+
 	
-	public void setName(String name) {
-		this.username = name;
-	}
+	// Change Person's user name
+	public void setUserName(String uname) { this.username = uname; }
+
+	// Change Person's password
+	public void setPassword(String passwd) { this.password = passwd; }
 	
-	public String getUsername() {
-		return username;
-	}
 	
-	public void setUsername(String username) {
-		this.username = username;
-	}
+	// Returns Person's first name
+	public String getFirst() { return this.firstName; }
+
+	// Returns Person's last name
+	public String getLast() { return this.lastName; }
+
+	
+	// Returns Person's user name
+	public String getUserName() { return this.username; }
+
+	// Returns Person's password
+	public String getPassword() { return this.password; }
 }
