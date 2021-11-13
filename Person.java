9320fac5b6363b530ad00d2d916da8018bd4f13e
Chandler Chang <78763922+Chandler-Chang@users.noreply.github.com>
2021-11-12 18:29:37 -0700

    Added ID number

--- Person.java
+++ Person.java
@@ -3,6 +3,7 @@ public class Person {
 	// Each person has first/last name, user name and password
 	protected String firstName, lastName;
 	protected String username, password;
+	protected int id;
 	
 	// Default Person Constructor
 	public Person() {
@@ -10,14 +11,16 @@ public class Person {
 		this.lastName = "";
 		this.username = "";
 		this.password = "";
+		this.id = "";
 	}
 		
 	// Constructor of Person with first and last name, user name and password
-	public Person(String First, String Last, String uname, String passwd) {
+	public Person(String First, String Last, String uname, String passwd, int id) {
 		this.firstName = First;
 		this.lastName = Last;
 		this.username = uname;
 		this.password = passwd;
+		this.id = id;
 	}
 	
 	// Change Person's first name
@@ -33,6 +36,9 @@ public class Person {
 	// Change Person's password
 	public void setPassword(String passwd) { this.password = passwd; }
 	
+	// Set Person's ID
+	public void setID(int id) { this.id = id; }
+	
 	
 	// Returns Person's first name
 	public String getFirst() { return this.firstName; }
@@ -40,10 +46,12 @@ public class Person {
 	// Returns Person's last name
 	public String getLast() { return this.lastName; }
 
-	
 	// Returns Person's user name
 	public String getUserName() { return this.username; }
 
 	// Returns Person's password
 	public String getPassword() { return this.password; }
+	
+	// Returns Person's password
+	public int getID() { return this.id; }
 }
